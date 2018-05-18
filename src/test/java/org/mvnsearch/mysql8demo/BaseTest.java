c350ce14fcbe6d38339ee55bac89fa8e57caf321
linux_china <linux_china@hotmail.com>
2018-05-17 18:35:21 -0700

    base test

---
+++ src/test/java/org/mvnsearch/mysql8demo/BaseTest.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.mysql8demo;
+
+import com.github.database.rider.spring.api.DBRider;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+/**
+ * base test
+ *
+ * @author linux_china
+ */
+@SpringJUnitConfig(Mysql8DemoApplication.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@DBRider
+public abstract class BaseTest {
+}
