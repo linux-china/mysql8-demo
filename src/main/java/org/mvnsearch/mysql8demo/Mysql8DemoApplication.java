3745d4c57bc79746090a5214dd0628e453233900
linux_china <linux_china@hotmail.com>
2018-05-17 18:34:56 -0700

    Mysql 8 demo app

---
+++ src/main/java/org/mvnsearch/mysql8demo/Mysql8DemoApplication.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.mysql8demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mysql8DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mysql8DemoApplication.class, args);
+	}
+
+}
