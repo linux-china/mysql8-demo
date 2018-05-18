0aecb6cb2c682867fa9c13fe8732e14ced196fae
linux_china <linux_china@hotmail.com>
2018-05-17 18:34:42 -0700

    flyway migration script

---
+++ src/main/resources/db/migration/V20180426203014__demo.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS account;
+create table account (
+  id         INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nick       varchar(16),
+  password   varchar(32),
+  created_at datetime,
+  updated_at datetime
+) DEFAULT CHARSET utf8;
